d2aa335a8d5c43e570ab2115344eb140aa4f2448
Denis Kuizinas <24dkuizinas@winstanleyschool.org.uk>
2025-12-31 10:03:56 +0000

    Implement window management store using Zustand

---
+++ store/useWindowStore.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+export const useWindowStore = create((set) => ({
+  windows: [],
+
+  openWindow: (app) =>
+    set((state) => ({
+      windows: [...state.windows, { id: Date.now(), app }]
+    })),
+
+  closeWindow: (id) =>
+    set((state) => ({
+      windows: state.windows.filter((w) => w.id !== id)
+    }))
+}));
