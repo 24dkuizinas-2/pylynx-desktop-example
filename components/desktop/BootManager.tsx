2a6d30365f59e701543646c0db04375d87df6061
Denis Kuizinas <24dkuizinas@winstanleyschool.org.uk>
2026-01-01 12:45:11 +0000

    Update BootManager.tsx

--- components/desktop/BootManager.tsx
+++ components/desktop/BootManager.tsx
@@ -6,11 +6,11 @@ import BiosScreen from "./BiosScreen";
 import BootLogs from "./BootLogs";
 import BootSequence from "./BootSequence";
 import FirstLaunchOverlay from "./FirstLaunchOverlay";
+
 export default function BootManager() {
   const [stage, setStage] = useState<
-  "bootloader" | "bios" | "logs" | "sequence" | "overlay" | "done"
->("bootloader");
-
+    "bootloader" | "bios" | "logs" | "sequence" | "overlay" | "done"
+  >("bootloader");
 
   return (
     <>
@@ -31,6 +31,10 @@ export default function BootManager() {
 
       {stage === "sequence" && (
         <BootSequence onDone={() => setStage("overlay")} />
+      )}
+
+      {stage === "overlay" && (
+        <FirstLaunchOverlay onDone={() => setStage("done")} />
       )}
     </>
   );
