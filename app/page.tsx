54ef2766c76c4d1fe795e7c812ef9ff4bcf203a8
Denis Kuizinas <24dkuizinas@winstanleyschool.org.uk>
2026-01-01 13:48:31 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
 "use client";
-import BootManager from "@/components/desktop/BootManager";
-import FirstLaunchOverlay from "@/components/desktop/FirstLaunchOverlay";
-import DesktopBackground from "@/components/desktop/DesktopBackground";
-import TopBar from "@/components/desktop/TopBar";
-import Dock from "@/components/desktop/Dock";
-import WindowManager from "@/components/windows/WindowManager";
 
-export default function Home() {
-  return (
-    <main className="w-screen h-screen overflow-hidden relative bg-black">
-      <BootManager />
-      <DesktopBackground />
-      <TopBar />
-      <WindowManager />
-      <Dock />
-    </main>
-  );
+import BootManager from "@/components/desktop/BootManager";
+
+export default function Page() {
+  return <BootManager />;
 }
