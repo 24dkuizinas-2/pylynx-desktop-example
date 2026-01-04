b1a676e24010e7ef6811cae76409b8ec1cf536b6
Denis Kuizinas <24dkuizinas@winstanleyschool.org.uk>
2026-01-04 11:20:44 +0000

    idk

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 export function getSupabase() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!
   );
 }
+
+
 
