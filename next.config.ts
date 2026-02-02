4753d0e85e1161a2c6744c0be2b11fcde2e3175d
Denis Kuizinas <24dkuizinas@winstanleyschool.org.uk>
2026-02-02 07:47:29 +0000

    idk

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  {
-    "redirects": [
-        {
-            "source": "/ads.txt",
-            "destination": "https://srv.adstxtmanager.com/70772/deniskuizinas.space",
-            "permanent": true
-        }
-    ]
-}
+  redirects: async () => [
+    {
+      source: "/ads.txt",
+      destination: "https://srv.adstxtmanager.com/70772/deniskuizinas.space",
+      permanent: true
+    }
+  ]
 };
 
 export default nextConfig;
